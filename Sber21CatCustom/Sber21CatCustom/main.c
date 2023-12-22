386bedc22120da2a86a8de2bafd0fd47a377a7d1
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-22 04:17:11 +0300

    Start a new project

---
+++ Sber21CatCustom/Sber21CatCustom/main.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  Sber21CatCustom
+//
+//  Created by Арсентий Халимовский on 22.12.2023.
+//
+
+// MARK: S21_cat
+
+/*
+ 
+ --Long Agruments
+ -n Number of strings
+ -b Number of non-empty strings
+ -s Skipping of unnecessary strings
+ -E $ at the end of string
+ -T ^| instead of tabulation
+ -v Выводит символы с кодами <= 31 через ^+64
+ - код 127 через ^? Кроме табуляции и конца строкки */
+
+#include <stdio.h>
+#include <getopt.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
